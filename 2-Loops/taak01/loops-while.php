523bc8da77f9327bea3f8c3cd5afa501c6bcd5b7
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2021-03-24 11:16:11 +0100

    save changes

---
+++ 2-Loops/taak01/loops-while.php
@@ -0,0 +1,12 @@
+<?php
+$leeftijd=0;
+
+
+while($leeftijd < 18){
+echo  "Ik ben  ".$leeftijd."  jaar, dus ik mag nog niet stemmen";
+echo"</br>";
+$leeftijd++;
+}
+echo "Ik ben ".$leeftijd." jaar oud dus ik heb stemrecht!"
+
+?>
