ab4d4af71b05b176506decaf7e08ab47464fe348
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2021-03-24 10:23:52 +0100

    save

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,9 @@
+<?php
+$voornaam= array("Piet"=>"Piet", "Muhammad"=>"Muhammad","Els"=>"Els");
+$achternaam=array("Piet"=>"Van Gogh", "Muhammad"=>"Ali","Els"=>"van den berg");
+$leeftijd= array("Piet"=>28, "Muhammad"=>17,"Els"=>24);
+$klas=array("Piet"=>"3F", "Muhammad"=>"9G", "Els"=>"0C");
+
+echo "Gegevens Muhammad: Voornaam: ".$voornaam["Muhammad"]." Achternaam: ".$achternaam["Muhammad"]." Leeftijd: ".$leeftijd["Muhammad"]." Klas: ".$klas["Muhammad"];
+
+?>
