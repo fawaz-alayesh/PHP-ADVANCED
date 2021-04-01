40865fb1a6f486a33eba8beb6e52015d2a633944
fawaz-alayesh <fawaz.alayesh@student.rocva.nl>
2021-04-01 12:19:11 +0200

    save

---
+++ 2-Loops/taak04/loops-foreach-deel2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<style>
+#form{
+  height:300px;
+  width:300px;
+  border: 1px solid black;
+  padding-left:10%;
+  padding-top:6%;
+}
+#btn{
+  height:30px;
+  width:60px;
+
+}
+#option{
+  height:30px;
+  width:60px;
+}
+</style>
+<form id="form" >   
+<label for="klassen"></label>
+<select name="klassen">
+<option value="">  </option>   
+    <?php  
+$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
+echo "Ik zit in klas: ";
+foreach($klassen as $value){
+  echo'<option value="'.$klassen.'">'.$value.'</option>';
+}
+
+?> 
+</select> 
+</br><input id="btn" type="submit">
+</form>
+
+
+</body>
+</html>
+
